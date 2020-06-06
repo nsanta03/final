67badb4ccf96743ed4d84eb377884ae87a9cccb5
nsanta03 <nick.santanjelo@gmail.com>
2020-06-06 04:19:27 +0000

    commit places db

--- createdb.rb
+++ createdb.rb
@@ -7,7 +7,7 @@ DB = Sequel.connect(connection_string)                                          
 # Database schema - this should reflect your domain model
 DB.create_table! :places do
   primary_key :id
-  String :name
+  String :title
   String :description, text: true
   String :location
 end
@@ -28,18 +28,18 @@ end
 # Insert initial (seed) data
 places_table = DB.from(:places)
 
-places_table.insert(name: "Fenway Park", 
+places_table.insert(title: "Fenway Park", 
                     description: "Visit America's most famous ballpark! Get tickets to a Red Sox game and come early to explore Yawkey Way. Stay after the game for drinks and live music on Boylston Street.",
                     location: "4 Jersey Street, Boston, MA")
 
-places_table.insert(name: "Mike's Pastry", 
+places_table.insert(title: "Mike's Pastry", 
                     description: "The best Italian pastries in Boston's classic North End! No trip to Boston is complete without a stop at Mike's.",
                     location: "300 Hanover Street, Boston, MA")
                     
-places_table.insert(name: "Legal Harborside", 
+places_table.insert(title: "Legal Harborside", 
                     description: "Rooftop views of the harbor and some of the best seafood in Boston! When you're done, take a walk and check out the swanky bars in this up and coming neighborhood.",
                     location: "270 Northern Avenue, Boston, MA")                    
 
-places_table.insert(name: "Gourmet Dumpling House", 
+places_table.insert(title: "Gourmet Dumpling House", 
                     description: "The best soup dumplings (xiao long bao) on the East Coast! Get here early and expect to wait for a table at this authentic classic spot.",
                     location: "52 Beach Street, Boston, MA")   
