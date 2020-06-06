7c46326cdf9b08f5b18c7b0b94db1632047f35d9
nsanta03 <nick.santanjelo@gmail.com>
2020-06-06 05:34:52 +0000

    commit

--- app.rb
+++ app.rb
@@ -18,11 +18,10 @@ after { puts; }                                                                 
 places_table = DB.from(:places)
 users_table = DB.from(:users)
 reviews_table = DB.from(:reviews)
-# rsvps_table = DB.from(:rsvps)
 
-#before do 
-#    @current_user = users_table.where(:id => session[:user]).to_a[0]
-#end
+before do 
+    @current_user = users_table.where(:id => session[:user]).to_a[0]
+end
 
 #Home Page (Nick's Local Boston Favorite Places)
 get "/" do 
@@ -38,3 +37,19 @@ get "/places/:id" do
     @count = reviews_table.where(:place_id => params["id"], :recommend => true).count
     view "place"
 end
+
+#Write a Review Page
+get "/places/:id/reviews/new" do
+    @place = places_table.where(:id => params["id"]).to_a[0]
+    view "new_review"
+end
+
+#Confirm review submitted
+post "/places/:id/reviews/create" do
+    reviews_table.insert(:event_id => params["id"],
+                       :recommend => params["recommend"],
+                       :user_id => @current_user[:id],
+                       :comments => params["comments"])
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    view "create_review"
+end 
